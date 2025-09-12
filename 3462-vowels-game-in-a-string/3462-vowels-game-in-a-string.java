f553a88fd6cb8bad5f2586fc928a1ccb2f979f0e
rishu5110 <157588332+rishu5110@users.noreply.github.com>
2025-09-12 11:03:39 +0530

    Time: 8 ms (45.41%), Space: 45.4 MB (82.68%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean doesAliceWin(String s) 
+    {
+           int size=s.length();
+           int count=0;
+       List<Integer> vowelsIdx = new ArrayList<>();
+           for(int i=0;i<size;i++)
+           {
+            char c=s.charAt(i);
+            if(c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u')
+            {   
+                count++;
+
+                vowelsIdx.add(i);
+
+            }
+           } 
+           if(count%2==0 && count!=0 || count%2==1)
+           {
+            return true;
+           }
+           if(count==0)
+          {
+            return false;
+          }
+           return false;
+    }
+
+}
