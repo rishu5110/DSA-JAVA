25c2d6ea47ff12c0ba9e6f39a8c859e64c18bd58
rishu5110 <157588332+rishu5110@users.noreply.github.com>
2026-02-11 19:14:31 +0530

    Time: 2 ms (97.63%), Space: 45.3 MB (86.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -19,22 +19,48 @@ class Solution {
     //  }
     //  return maxlen;
 
-    
-    int maxLen=0;
-    for(int i=0;i<s.length();i++)
-    {
-        int[] hash = new int[256];
-        for(int j=i;j<s.length();j++)
-        {
-            
-            if(hash[s.charAt(j)]==1) break;
-            int len=j-i+1;
-            maxLen=Math.max(len,maxLen);
-            hash[s.charAt(j)]=1;
 
+    int l=0;    //Time complexity: O(n)
+    int r=0;
+    int n=s.length();
+    int hash[]= new int[256];
+    Arrays.fill(hash,-1);
+    int maxLength=0;
+    
+        while(r<n)
+        {
+            if(hash[s.charAt(r)]>=l)
+            {
+                l=Math.max(hash[s.charAt(r)]+1,l);
+
+            }
+            int len=r-l+1;
+             maxLength=Math.max(len,maxLength);
+            hash[s.charAt(r)]=r;
+            r++;
         }
-    }
-    return maxLen;
+
+        return maxLength;
+
+
+    
+
+    
+    // int maxLen=0;    //Time Complexity:O(N*N)
+    // for(int i=0;i<s.length();i++)
+    // {
+    //     int[] hash = new int[256];
+    //     for(int j=i;j<s.length();j++)
+    //     {
+            
+    //         if(hash[s.charAt(j)]==1) break;
+    //         int len=j-i+1;
+    //         maxLen=Math.max(len,maxLen);
+    //         hash[s.charAt(j)]=1;
+
+    //     }
+    // }
+    // return maxLen;
 
 
     }
